8c706f414ecbdfc71e41f209cc91eab2d75ed8dd
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-05-29 18:56:43 +0300

    disiabel pointer none

--- src/component/Demo.js
+++ src/component/Demo.js
@@ -17,21 +17,26 @@ const Input = styled.input`
   border-radius: 5px;
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
   width: 300px;
+  outline: 10px solid #fff;
+
 `;
 
 const Button = styled.button`
   position: absolute;
   top: 0;
   right: 0;
-  background-color: #007bff;
+  background-image: linear-gradient(to right, #ffad00, #ff7300, #ffad00);
+
   color: #fff;
   padding: 10px;
   border: none;
   border-radius: 5px;
   cursor: pointer;
+  outline: none;
   &:hover {
-    background-color: #0062cc;
+    background-image: linear-gradient(to right, #ff7300, #ffad00, #ffad00);
   }
+
 `;
   return (
     <div>
